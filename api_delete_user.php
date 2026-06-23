3d56dfeae3c90744925240fed99d5d2f80d602f1
Ajman <onlinebizz2333@gmail.com>
2026-06-23 23:34:17 +0800

    Completed User Management Module

--- api_delete_user.php
+++ api_delete_user.php
@@ -17,6 +17,69 @@ include "db.php";
 $id =
 (int) $_POST["id"];
 
+
+
+$stmt =
+$conn->prepare(
+"
+SELECT role
+FROM users
+WHERE id = ?
+"
+);
+
+$stmt->bind_param(
+    "i",
+    $id
+);
+
+$stmt->execute();
+
+$result =
+$stmt->get_result();
+
+$user =
+$result->fetch_assoc();
+
+
+
+if (
+    $user["role"]
+    ===
+    "admin"
+) {
+
+    $result =
+    mysqli_query(
+        $conn,
+        "
+        SELECT COUNT(*)
+        AS total_admins
+
+        FROM users
+
+        WHERE role = 'admin'
+        "
+    );
+
+    $row =
+    mysqli_fetch_assoc(
+        $result
+    );
+
+    if (
+        $row["total_admins"]
+        <= 1
+    ) {
+
+        die(
+            "Cannot delete last admin."
+        );
+
+    }
+
+}
+
 if (
     $id ==
     $_SESSION["user_id"]
